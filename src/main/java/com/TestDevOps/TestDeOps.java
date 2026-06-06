631b0d74765454498322010beece0bc2ac2abb7f
kuldip patil <Kuldippatil228@gmail.com>
2026-06-06 12:15:13 +0530

    firstcommit

---
+++ src/main/java/com/TestDevOps/TestDeOps.java
@@ -0,0 +1,13 @@
+package com.TestDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeOps {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeOps.class, args);
+	}
+
+}
